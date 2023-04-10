ad9ba64cc84c69c6752c11097c9d2cc48d7377e0
Eduardo Mínguez <e.minguez@gmail.com>
2023-04-10 17:31:01 +0200

    First commit

---
+++ slemicro/create_combustion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create a temp dir to host the assets
+TMPDIR=$(mktemp -d)
+
+# Create required folders
+mkdir -p ${TMPDIR}/{combustion,ignition}
+
+# Copy the combustion script
+cp script ${TMPDIR}/combustion/script
+
+# Convert the config.fcc yaml file to ignition
+butane -p -o ${TMPDIR}/ignition/config.ign config.fcc
+
+# Create an iso
+mkisofs -full-iso9660-filenames -o combustion.iso -V combustion ${TMPDIR}
+
+# Remove leftovers
+rm -Rf ${TMPDIR}
