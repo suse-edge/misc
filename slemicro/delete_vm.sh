bd8978a225b979ba0c1fe8299ac1a443c2b2adf8
e-minguez <e.minguez@gmail.com>
2023-05-09 17:27:00 +0200

    Fix tabs for heredocs with dash
    Reasoning: https://www.oreilly.com/library/view/bash-cookbook/0596526784/ch03s04.html

--- slemicro/delete_vm.sh
+++ slemicro/delete_vm.sh
@@ -3,38 +3,36 @@ set -euo pipefail
 BASEDIR="$(dirname "$0")"
 
 die(){
-  echo ${1}
-  exit ${2}
+	echo ${1}
+	exit ${2}
 }
 
 # Get the env file
 source ${BASEDIR}/.env
 if [ $# -eq 1 ]; then
-    VMNAME=$1
+	VMNAME=$1
 fi
 VMFOLDER="${VMFOLDER:-~/VMs}"
 
 if [ $(uname -o) == "Darwin" ]; then
 # Stop and delete the VM using UTM
-  OUTPUT=$(
-  osascript <<-END
-  tell application "UTM"
-	  set vm to virtual machine named "${VMNAME}"
-	  if status of vm is started then stop vm
-	  repeat
-	    if status of vm is stopped then exit repeat
-    end repeat
-	  delete virtual machine named "${VMNAME}"
-  end tell
-  END
-  )
+	OUTPUT=$(
+	osascript <<-END
+	tell application "UTM"
+		set vm to virtual machine named "${VMNAME}"
+		if status of vm is started then stop vm
+		repeat
+			if status of vm is stopped then exit repeat
+		end repeat
+		delete virtual machine named "${VMNAME}"
+	end tell
+	END)
 elif [ $(uname -o) == "GNU/Linux" ]; then
-  virsh destroy ${VMNAME}
-  virsh undefine ${VMNAME}
+	virsh destroy ${VMNAME}
+	virsh undefine ${VMNAME}
 else
-  die "Unsupported operating system" 2
+	die "Unsupported operating system" 2
 fi
-
 
 [ -f ${VMFOLDER}/${VMNAME}.raw ] && rm -f ${VMFOLDER}/${VMNAME}.raw
 [ -f ${VMFOLDER}/ignition-and-combustion-${VMNAME}.iso ] && rm -f ${VMFOLDER}/ignition-and-combustion-${VMNAME}.iso
