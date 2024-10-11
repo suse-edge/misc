cc24e534f1988c22cf63ec4641d5ffbef332a924
e-minguez <e.minguez@gmail.com>
2024-10-11 11:57:10 +0200

    Improved scripts to support SCC pagination

--- scc/delete_unseen_systems.sh
+++ scc/delete_unseen_systems.sh
@@ -12,6 +12,34 @@ usage(){
 	echo "7 days by default"
 }
 
+delete_systems(){
+	SYSTEMS=$(curl -X 'GET' -s "${1}" -H 'accept: application/json' -u ${USER}:${PASS})
+	TOTAL=$(echo ${SYSTEMS} | jq length)
+
+	if [ ${TOTAL} -le 0 ]; then
+		# Not an error but a "warn"
+		die "Warning: No systems found" 0
+	fi
+
+	TODAY=$(date +%s)
+
+	for ((j=0; j<${TOTAL}; j++)); do
+		HOST=$(echo ${SYSTEMS} | jq -r ".[${j}]")
+		ID=$(echo ${HOST} | jq -r ".id")
+		HOSTLOGIN=$(echo ${HOST} | jq -r ".login")
+		HOSTPASS=$(echo ${HOST} | jq -r ".password")
+		LASTSEEN=$(echo ${HOST} | jq -r ".last_seen_at")
+		EPOCH=$(date -d "${LASTSEEN}" +%s)
+		let DIFF=(${TODAY}-${EPOCH})/86400
+		if [ ${DIFF} -gt ${DAYS} ]; then
+			echo "Deleting $((j+1))/${TOTAL} - ${ID}, last seen at ${LASTSEEN}"
+			curl -X 'DELETE' -s 'https://scc.suse.com/connect/systems' \
+				-H 'Accept: application/vnd.scc.suse.com.v4+json' \
+				-u ${HOSTLOGIN}:${HOSTPASS}
+		fi
+	done
+}
+
 command -v jq > /dev/null 2>&1 || die "jq not found" 2
 
 if [ $# -lt 2 ]; then
@@ -22,26 +50,22 @@ fi
 USER=${1}
 PASS=${2}
 DAYS="${3:-7}"
+URL="https://scc.suse.com/connect/organizations/systems"
 
-SYSTEMS=$(curl -X 'GET' -s 'https://scc.suse.com/connect/organizations/systems' -H 'accept: application/json' -u ${USER}:${PASS})
-TOTAL=$(echo ${SYSTEMS} | jq length)
+PAGINATION=$(curl "${URL}" -H 'accept: application/json' -u ${USER}:${PASS} -I -s -o /dev/null -w '%header{link}')
 
-if [ ${TOTAL} -le 0 ]; then
-	# Not an error but a "warn"
-	die "Warning: No systems found" 0
+if [[ ${#PAGINATION} -gt 0 ]]; then
+	# Extract the "last" page number
+	LASTPAGE=$(echo "${PAGINATION}" | grep -oP '(?<=page=)\d+(?=>; rel="last")')
+	# Extract the "next" page number
+	NEXTPAGE=$(echo "${PAGINATION}" | grep -oP '(?<=page=)\d+(?=>; rel="next")')
+	# Start the loop from NEXT - 1
+	STARTPAGE=$((NEXTPAGE - 1))
+
+	# Loop from START to LAST
+	for ((i=STARTPAGE; i<=LASTPAGE; i++)); do
+		delete_systems "${URL}?page=${i}"
+	done
+else
+	delete_systems "${URL}"
 fi
-
-TODAY=$(date +%s)
-
-for ((i=0; i<${TOTAL}; i++)); do
-	LOGIN=$(echo ${SYSTEMS} | jq -r ".[${i}].login")
-	PASS=$(echo ${SYSTEMS} | jq -r ".[${i}].password")
-	LAST=$(echo ${SYSTEMS} | jq -r ".[${i}].last_seen_at")
-	EPOCH=$(date -d "${LAST}" +%s)
-	let DIFF=(${TODAY}-${EPOCH})/86400
-	if [ ${DIFF} -gt ${DAYS} ]; then
-		curl -X 'DELETE' -s 'https://scc.suse.com/connect/systems' \
-		 	-H 'Accept: application/vnd.scc.suse.com.v4+json' \
-		 	-u ${LOGIN}:${PASS}
-	fi
-done
